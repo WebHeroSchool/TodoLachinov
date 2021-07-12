bc26e8b6f28b24218602e7d448435bc4951a15df
EmilLachinov8 <lachinov_em@mail.ru>
2021-07-12 17:03:41 +0500

    jsx3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+<<<<<<< Updated upstream
 const element = (<div>
   Hello world!
   <a href="">Button</a>
 </div>);
 
 export { element };
+=======
+const count = 88;
+const perem = 'Переменная';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+         <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p style={{
+            color: 'blue'
+          }}>
+            Hello world!
+          </p>
+          <p> {perem} </p>
+          <p> {count} </p>
+          <p> {43 + 23} </p>
+          <p> {23 > 50 || 90 > 50} </p>
+          <p> {{count} > 25 ? 'меньше' : 'больше'} </p>
+          <p> {undefined} {null} {false} {true}</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }  
+}
+
+export default App;
+>>>>>>> Stashed changes
--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+<<<<<<< Updated upstream
 import { element } from './App';
 
 ReactDOM.render(element, document.getElementById('root'));
+=======
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+reportWebVitals();
+>>>>>>> Stashed changes
