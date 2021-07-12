522f469efe0854267d4755111365a21e3d6237b2
EmilLachinov8 <lachinov_em@mail.ru>
2021-07-12 22:31:29 +0500

    Structure

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< Updated upstream
-import { element } from './App';
-
-ReactDOM.render(element, document.getElementById('root'));
-=======
 import App from './components/App/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
->>>>>>> Stashed changes
+
