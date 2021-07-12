fa48505604cab9530b445362d9fdefcbd2940191
EmilLachinov8 <lachinov_em@mail.ru>
2021-07-12 16:04:52 +0500

    JSX

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react';
 
 const element = (<div>
@@ -6,3 +7,56 @@ const element = (<div>
 </div>);
 
 export { element };
+=======
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const count = 88;
+const perem = 'Переменная';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+         <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p style={{
+            color: 'blue'
+          }}>
+            Hello world!
+          </p>
+          <p> {perem} </p>
+          <p> {count} </p>
+          <p> {43 + 23} </p>
+          <p> {23 > 50 || 90 > 50} </p>
+          <p> {{count} > 25 ? 'меньше' : 'больше'} </p>
+          <p> {undefined} {null} {false} {true}</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }  
+}
+
+export default App;
+=======
+import React from 'react';
+
+const element = (<div>
+  Hello world!
+  <a href="">Button</a>
+</div>);
+
+export { element };
+>>>>>>> Stashed changes
--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+<<<<<<< Updated upstream
 import { element } from './App';
 
+=======
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+reportWebVitals();
+
+import { element } from './App';
+
+>>>>>>> Stashed changes
 ReactDOM.render(element, document.getElementById('root'));
