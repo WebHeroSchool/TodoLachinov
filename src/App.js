97347a030e5dfb3ddcb880457a59dd2654da7239
EmilLachinov8 <lachinov_em@mail.ru>
2021-07-15 17:47:03 +0500

    jsx4

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const element = (<div>
-  Hello world!
-  <a href="">Button</a>
-</div>);
+const count = 88;
+const perem = 'Переменная';
 
-const ItemList = () => (<ul>
-  <li>1</li>
-  <li>2</li>
-  <li>3</li>
-</ul>);
-
-const App = () => (<div> 
-  <h1>Hello world!</h1>
-  <ItemList />
-</div>);
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+         <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p style={{
+            color: 'blue'
+          }}>
+            Hello world!
+          </p>
+          <p> {perem} </p>
+          <p> {count} </p>
+          <p> {43 + 23} </p>
+          <p> {23 > 50 || 90 > 50} </p>
+          <p> {{count} > 25 ? 'меньше' : 'больше'} </p>
+          <p> {undefined} {null} {false} {true}</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }  
+}
 
 export default App;
-
