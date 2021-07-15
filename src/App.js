55adf406468967c512faad0b4b9b358a733ac76b
EmilLachinov8 <lachinov_em@mail.ru>
2021-07-15 17:53:38 +0500

    modules4

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import React from 'react';
 
-const element = (<div>
-  Hello world!
-  <a href="">Button</a>
-</div>);
+import React, { Component } from 'react';
+import {count, length} from './number';
 
-const ItemList = () => (<ul>
-  <li>1</li>
-  <li>2</li>
-  <li>3</li>
-</ul>);
-
-const App = () => (<div> 
-  <h1>Hello world!</h1>
-  <ItemList />
-</div>);
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+         <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p style={{
+            color: 'blue'
+          }}>
+            Hello world!
+          </p>
+          <p> Результат: {count*length} </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }  
+}
 
 export default App;
-
