f062a418c2bd7c05a358a60eb6585e41a2843c96
EmilLachinov8 <lachinov_em@mail.ru>
2021-07-15 18:44:56 +0500

    Styles2

---
+++ src/components/App/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ItemList from '../ItemList/ItemList';
+import InputItem from '../InputItem/InputItem';
+import Footer from '../Footer/Footer';
+import './App.css';
+
+const todoItem = 'Make the App';
+const counter = 3;
+
+const App = () => {
+  const items = [
+    {
+      value: 'Make the app',
+    },
+    {
+      value: 'Check it',
+    },
+    {
+      value: 'Make pull request',
+    }
+  ];
+
+  return (<div className = "wrap"> 
+    <h1 className = "wrap__title">Important actions:</h1>
+    <InputItem />
+    <ItemList items = {items} />
+    <Footer counter = {counter} />
+  </div>);
+}
+
+export default App;
