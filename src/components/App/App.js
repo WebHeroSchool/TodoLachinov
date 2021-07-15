bd6aa67b9af9ad7bffd3de5b63442a1f01b070a2
EmilLachinov8 <lachinov_em@mail.ru>
2021-07-15 18:20:34 +0500

    Props2

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ItemList from '../ItemList/ItemList';
+import InputItem from '../InputItem/InputItem';
+import Footer from '../Footer/Footer';
+
+const todoItem = 'Make the App';
+const counter = 3;
+
+const App = () => (<div> 
+  <h1>Important actions:</h1>
+  <InputItem />
+  <ItemList todoItem = {todoItem}/>
+  <Footer counter = {counter} />
+</div>);
+
+export default App;
