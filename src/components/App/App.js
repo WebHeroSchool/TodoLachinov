534c36951f68a1f3d66325e17e530987d55c0907
EmilLachinov8 <lachinov_em@mail.ru>
2021-07-18 17:10:31 +0500

    Styles change

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+<<<<<<< Updated upstream
 import ItemList from '../itemList/itemList';
 import InputItem from '../InputItem/inputItem';
 import Footer from '../Footer/footer';
+=======
+import ItemList from '../ItemList/ItemList';
+import InputItem from '../InputItem/InputItem';
+import Footer from '../Footer/Footer';
+>>>>>>> Stashed changes
 import './App.css';
 
 const todoItem = 'Make the App';
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Footer = ({ counter }) => (<div> The number of actions left: { counter }</div>);
+
+export default Footer;
