f5834b2a0c22e91878568eb37ba62341a252c0be
EmilLachinov8 <lachinov_em@mail.ru>
2021-07-20 23:04:21 +0500

    ComponentClass

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,11 +3,33 @@ import ItemList from '../ItemList/ItemList';
 import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
 
-const App = () => (<div> 
-  <h1>Важные дела:</h1>
-  <InputItem />
-  <ItemList />
-  <Footer />
-</div>);
+const counter = 3;
+
+class App extends React.Component {
+  render() {
+      const items = [
+        {
+          value: 'Make the app',
+          isDone: true,
+        },
+        {
+          value: 'Check it',
+          isDone: false,
+        },
+        {
+          value: 'Make pull request',
+          isDone: false,
+        }
+      ];
+    
+      return (<div className = {styles.wrap}> 
+        <h1 className = {styles.title} >Important actions:</h1>
+        <InputItem />
+        <ItemList items = {items} />
+        <Footer counter = {counter} />
+      </div>);
+    }
+}
+  
 
 export default App;
