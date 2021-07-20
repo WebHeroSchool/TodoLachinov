59e70ed3dfa756b72e895f4cf251cf54fea1e603
EmilLachinov8 <lachinov_em@mail.ru>
2021-07-20 23:42:10 +0500

    Change comp-class

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,36 @@ import React from 'react';
 import ItemList from '../ItemList/ItemList';
 import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
+import styles from './App.module.css';
 
-const App = () => (<div> 
-  <h1>Важные дела:</h1>
-  <InputItem />
-  <ItemList />
-  <Footer />
-</div>);
+const counter = 3;
+
+class App extends React.Component {
+  render() {
+      const items = [
+        {
+          value: 'Make the app',
+          isDone: true,
+        },
+        {
+          value: 'Check it',
+          isDone: false,
+        },
+        {
+          value: 'Make pull request',
+          isDone: false,
+        }
+      ];
+    
+
+      return (<div className = {styles.wrap}> 
+        <h1 className = {styles.title} >Important actions:</h1>
+        <InputItem />
+        <ItemList items = {items} />
+        <Footer counter = {counter} />
+      </div>);
+    }
+}
+  
 
 export default App;
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Item from '../Item/Item';
+import Checkboxs from '../Checkbox/Checkbox';
+import DeleteIcons from '../Delete/Delete';
+import styles from './ItemList.module.css';
 
-
-const ItemList = () => (<ul>
-  <li><Item /></li>
-  <li><Item /></li>
-  <li><Item /></li>
+const ItemList = ({ items }) => (<ul>
+  {items.map(item => 
+  <li className={styles.item} key={item.value}> <Checkboxs /> 
+    <Item value={item.value} isDone={item.isDone} />
+    <DeleteIcons />
+  </li>)}
 </ul>);
 
 
