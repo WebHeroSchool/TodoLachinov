66afb61a17fadd3a8e71836a9ca7a888e9ab4399
EmilLachinov8 <70032787+EmilLachinov8@users.noreply.github.com>
2021-07-21 23:14:14 +0500

    Delete src/components/itemList directory

--- src/components/itemList/itemList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Item from '../item/item';
-
-const ItemList = ({ items }) => (<ul>
-  {items.map(item => <li key={item.value}> <Item value={item.value} /></li>)}
-</ul>);
-
-
-export default ItemList;
