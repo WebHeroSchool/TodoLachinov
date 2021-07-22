6ded59c8256004ffdeb05d1cfe69698e8611a6de
EmilLachinov8 <70032787+EmilLachinov8@users.noreply.github.com>
2021-07-22 23:08:29 +0500

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
+const counter = 3;
 
 const App = () => {
   const items = [
