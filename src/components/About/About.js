5fee6fb19c5a305c9cdbd39d791337c1a5f9eb76
EmilLachinov8 <lachinov_em@mail.ru>
2021-09-27 22:52:59 +0500

    Добавил catch и имя, аву

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -22,32 +22,47 @@ class About extends React.Component {
 				repoList: data,
 				isLoading: false
 			});
-		},
-		(error) => {
+		})
+
+		.catch((error) => {
 			this.setState({
 				isLoading: false,
 				error
 			});
-		},
+		})
+
 		octokit.rest.users.getByUsername({
 			username: 'EmilLachinov8'
 		})
 		.then(({ data }) => {
 			this.setState({
+				name: data.login,
+        		bio: data.bio,
+				avatar: data.avatar_url,
 				userInfo: data,
 				isLoading: false
-			});
-		}))
+			})
+		})
+
+		.catch((error) => {
+			this.setState({
+				isLoading: false,
+				error
+			})
+		})
 	}
 
 	render() {
-		const { isLoading, repoList, error } = this.state;
+		const { isLoading, repoList, error, avatar, bio, name } = this.state;
 		if (error) {
 			return (<div> Error: {error.message} </div>)
 		} else {
 			return (
 				<div className={styles.box}>
-					<h1 className={styles.title}> {isLoading ? <Preload /> : about} </h1>
+					<h1> { name }</h1>
+					<img className={styles.avatar} src={avatar} alt="avatar"/>
+					<p>{ bio }</p>
+					<h2 className={styles.title}> {isLoading ? <Preload /> : about} </h2>
 					{!isLoading && <ol>
 						{repoList.map(repo => (<li key={repo.id}>
 							<a href={repo.html_url}>{repo.name}</a>
