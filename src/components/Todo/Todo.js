63cc2bb97c47b33f4e6e1085df38947fc5a60b5b
EmilLachinov8 <lachinov_em@mail.ru>
2021-11-01 01:50:12 +0500

    Final project

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,98 +1,156 @@
-import React from 'react';
-import ItemList from '../ItemList/ItemList';
-import InputItem from '../InputItem/InputItem';
-import Footer from '../Footer/Footer';
-import styles from './Todo.module.css';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
+import React, { useEffect, useState } from 'react';
+import InputItem from "../InputItem/InputItem";
+import ItemList from "../ItemList/ItemList";
+import Button from '@material-ui/core/Button';
+import styles from './ToDo.module.css';
+import classnames from 'classnames';
+import errorImg from './img/error.svg';
 
+const ToDo = () => {
+  const initialState = {
+    Items: [
+    ],
+    filter: 'all',
+    filteredItems: [],
+    count: 0,
+    countActive: 0,
+    countDone: 0,
+    lastID: 0,
+    selectedId: -1
+  };
 
-class Todo extends React.Component {
+  const [Items, setItems] = useState(initialState.Items);
+  const [filteredItems, setFilteredItems] = useState(initialState.filteredItems);
+  const [filter, setFilter] = useState(initialState.filter);
+  const [count, setCount] = useState(initialState.count);
+  const [lastID, setLastId] = useState(initialState.lastID);
+  const [countActive, setCountActive] = useState(initialState.countActive);
+  const [countDone, setCountDone] = useState(initialState.countDone);
+  const [selectedId, setSelectedId] = useState(initialState.selectedId);
 
-    state = {
-        error: false,
-        items: [
-            {
-                value: 'Make the app',
-                isDone: true,
-                id: 1
-            },
-            {
-                value: 'Check it',
-                isDone: false,
-                id: 2
-            },
-            {
-                value: 'Make pull request',
-                isDone: false,
-                id: 3
-            }
-        ],
-    };
+  useEffect(() => {
+    setFilteredItems(Items);
+  }, []);
 
-    onClickDone = id => {
-        const newItemList = this.state.items.map(item => {
-            const newItem = { ...item };
-            if (item.id === id) {
-                newItem.isDone = !item.isDone;
-            }
+  useEffect(() => {
+    onClickFilter(filter);
 
-            return newItem;
-        })
+  }, [Items]);
 
-        this.setState({ items: newItemList });
-    };
+  useEffect(() => {
+    onClickSelected(-1);
+  }, [filter]);
 
-    onClickDelete = id => {
-        const newItemList = this.state.items.filter(item => item.id !== id);
-        this.setState({ items: newItemList });
+  const onClickDone = id => {
+    const newItemList = Items.map(item => {
+      const newItem = { ...item };
+
+      if (item.id === id) {
+        newItem.isDone = !item.isDone;
+      }
+      return newItem;
+    });
+
+    const newCountDone = newItemList.filter(item => item.isDone);
+    const newCountActive = newItemList.filter(item => !item.isDone);
+
+    setItems(newItemList);
+    setCountDone(newCountDone.length);
+    setCountActive(newCountActive.length)
+  };
+
+  const onClickDelete = id => {
+    const newItemList = Items.filter(item => item.id !== id);
+    const newCountDone = newItemList.filter(item => item.isDone);
+    const newCountActive = newItemList.filter(item => !item.isDone);
+
+    setItems(newItemList);
+    setCount((count) => count - 1);
+    setCountDone(newCountDone.length);
+    setCountActive(newCountActive.length);
+  };
+
+  const onClickSelected = id => setSelectedId(id);
+
+  const onClickAdd = (value) => {
+    const newItemList = [
+      ...Items,
+      {
+        value,
+        isDone: false,
+        id: lastID + 1
+      }
+    ];
+
+    setItems(newItemList);
+    setCount((count) => count + 1);
+    setLastId((lastID) => lastID +1);
+    setCountActive((countActive) => countActive + 1);
+  };
+
+  const onClickFilter = filter => {
+    let  newItemList = [];
+    switch (filter) {
+      case 'all':
+        newItemList = Items;
+        break;
+      case 'active':
+        newItemList = Items.filter (item => !item.isDone);
+        break;
+      case 'finished':
+        newItemList = Items.filter (item => item.isDone);
+        break;
+      default:
+        newItemList = Items;
     }
 
+    setFilteredItems(newItemList);
+    setFilter(filter);
+  };
 
-    onClickAdd = value => {
-        if ( value !== '') {
+  return (
+    <div className={styles.wrap}>
+      <h1 className={styles.title}>Important things:</h1>
+      <div className={styles.wrapList}>
 
-			this.setState(state => ({
-				items: [
-					...state.items,
-					{
-						value,
-						isDone: false,
-						id: state.count + 1
-					}
-				],
-				count: state.count + 1,
-				error: false
-			}));
-		} else {
-			this.setState(state => ({
-				error: true
-			}))
+        <InputItem onClickAdd={onClickAdd} Items={Items} className={styles.inputItem}/>
 
-		}
-	}
+        {Items.length > 0 ?
+        <ItemList
+          Items={filteredItems}
+          onClickDone={onClickDone}
+          onClickDelete={onClickDelete}
+          onClickSelected={onClickSelected}
+          selectedId={selectedId}
+          className={styles.itemList}
+        /> :
 
+        <div className={styles.TodoErrorWrap}>
+          <img src={errorImg} alt='error loading' className={styles.TodoErrorImg}></img>
+            <h2 className={styles.TodoErrorTitle}>You haven't added any task</h2>
+            <p className={styles.TodoErrorText}>Do it</p>
+        </div>}
 
-    render() {
-        const count = this.state.items.filter(item => item.isDone === false);
+      </div>
 
-        return (
-            <div className={styles.wrapper}>
-                <Card>
-                    <CardContent>
-                        <h1 className={styles.title}>Todo list</h1>
-                        <InputItem onClickAdd={this.onClickAdd} />
-                        <ItemList
-                            items={this.state.items}
-                            onClickDone={this.onClickDone}
-                            onClickDelete={this.onClickDelete}
-                        />
-                        <Footer count={count.length} />
-                    </CardContent>
-                </Card>
-            </div>
-        );
-    }
-}
+      <div className={styles.wrapFooter}>
+        <div className={styles.wrapFilter}>
+          <Button className={classnames({ [styles.todoCount]: true, [styles.todoCountActive]: filter === 'all' })} onClick={() => onClickFilter('all')} >
+            All
+            <span className={classnames({ [styles.todoCountNumber]: true, [styles.todoCountNumberActive]: filter === 'all' })}>&nbsp; {count}</span>
+          </Button>
+          <Button className={classnames({ [styles.todoCount]: true, [styles.todoCountActive]: filter === 'active' })} onClick={() => onClickFilter('active')}>
+            Active
+            <span className={classnames({ [styles.todoCountNumber]: true, [styles.todoCountNumberActive]: filter === 'active' })}>&nbsp; {countActive}</span>
+          </Button>
+          <Button className={classnames({ [styles.todoCount]: true, [styles.todoCountActive]: filter === 'finished' })} onClick={() => onClickFilter('finished')}>
+            Done
+            <span className={classnames({ [styles.todoCountNumber]: true, [styles.todoCountNumberActive]: filter === 'finished' })}>&nbsp; {countDone}</span>
+          </Button>
+        </div>
+      </div>
 
-export default Todo;
+    </div>);
+};
+
+export default ToDo;
